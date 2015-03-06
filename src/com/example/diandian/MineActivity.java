9d59949700ec356b1276d57f1903dfcd61715598
Qin Fei <370042621@qq.com>
2015-03-06 20:27:57 +0800

    20150306增加注销功能

--- src/com/example/diandian/MineActivity.java
+++ src/com/example/diandian/MineActivity.java
@@ -1,34 +1,34 @@
 package com.example.diandian;
 
+import com.example.diandian.R.id;
+
 import android.app.Activity;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MineActivity extends Activity {
-
+	private Button btnLogOut;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_mine);
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.mine, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
+		btnLogOut=(Button) findViewById(id.btnLogOut);
+		btnLogOut.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				SharedPreferences sharepf=getSharedPreferences("Login", MODE_PRIVATE);
+				Editor editor=sharepf.edit();
+				editor.remove("login_state");
+				editor.commit();
+				finish();
+			}
+		});
 	}
 }
